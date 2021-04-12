8fdd59a10f376cdcb6564cafbd07d05e4cbb5f91
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-04-12 17:06:42 +0200

    ajout du logo sur la homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './structurehomepage.css';
+import Homepage from './Components/stucturehomepage';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-<<<<<<< HEAD
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'></a>
-=======
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
->>>>>>> 8cac9c7e17ea0782c18ec90e372026d66485a9fb
-      </header>
-    </div>
-  );
+  return;
+  <div>
+    <Homepage />;
+  </div>;
 }
 
 export default App;
---
+++ src/Components/stucturehomepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './structurehomepage.css';
+import logo from './logo-oovy-png';
+
+export default function Homepage() {
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  );
+}
