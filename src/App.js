203d54eca3a57dd7ca709f35d5c5d47a1658e42a
Gregory-Bouteville <gregory.bouteville@orange.fr>
2021-04-14 17:21:29 +0200

    Creation du footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-<<<<<<< HEAD
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'></a>
-=======
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
->>>>>>> 8cac9c7e17ea0782c18ec90e372026d66485a9fb
-      </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>Conditions générales</h4>
+          </div>
+          <div className="col">
+            <h4>Medias Sociaux</h4>
+            <ul className="list-Medias">
+              <li>
+                <a href="https://www.instagram.com/?hl=fr" target="_blank">
+                  {' '}
+                  <img src="Instagram.png" alt="Instagram Icon" width="50px" height="50px" />{' '}
+                </a>{' '}
+              </li>
+              <li>
+                <a href="https://fr-fr.facebook.com/" target="_blank">
+                  {' '}
+                  <img src="Facebook.png" alt="Facebook Icon" width="50px" height="50px" />{' '}
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/?lang=fr" target="_blank">
+                  {' '}
+                  <img src="Twitter.png" alt="Twitter Icon" width="50px" height="50px" />{' '}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col">
+            <h4>
+              <a href="">À propos</a>
+            </h4>
+          </div>
+        </div>
+        <hr className="hr" />
+        <div className="row">
+          <p className="col-sm">&copy;{new Date().getFullYear()} Oovy France | Tous droits réservés | Mentions légales | Conditions d'utilisation</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
