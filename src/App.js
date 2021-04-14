b756cc9240c9970b1baf31978bbd3c04a8119a6b
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-04-14 23:04:50 +0200

    structure de la section A propos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutUs from './Components/AboutUs/AboutUs';
 import Headerallpages from './Components/headerallpages';
 
 function App() {
   return (
     <div>
       <Headerallpages />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './AboutUs.css';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h2 className="text1">A propos</h2>
+      <div className="contains"></div>
+    </div>
+  );
+}
