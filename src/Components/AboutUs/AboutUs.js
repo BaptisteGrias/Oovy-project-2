311ea164f428f78ae1b64b697090e3cb4701c4fe
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-04-15 15:13:56 +0200

    ajout du css

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -3,9 +3,13 @@ import './AboutUs.css';
 
 export default function AboutUs() {
   return (
-    <div>
-      <h2 className="text1">A propos</h2>
-      <div className="contains"></div>
+    <div className="apropos">
+      <h2 className="text1">A propos :</h2>
+      <div className="contains">
+        <p className="description">
+          lorem40bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb{' '}
+        </p>
+      </div>
     </div>
   );
 }
