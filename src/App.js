9039252fcd1297604486031a83d5a2d92f0eac27
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-04-19 15:17:52 +0200

    ajout react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Homepage from './Components/HomePage/Homepage';
 import Footer from './Components/Footer/Footer';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
