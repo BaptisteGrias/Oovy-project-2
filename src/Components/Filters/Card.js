3dca1ddb8e5a010015ecca57a2769c7d7988b99a
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-04-28 22:30:41 +0200

    ajout des trois cards qui affichent trois films

--- src/Components/Filters/Card.js
+++ src/Components/Filters/Card.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import "./Card.css";
+import './Card.css';
 
-const baseUrl = "https://api.themoviedb.org/3";
-const posterUrl = "https://image.tmdb.org/t/p/w200/"
+const baseUrl = 'https://api.themoviedb.org/3';
+const posterUrl = 'https://image.tmdb.org/t/p/w300/';
 const Card = ({ original_title, poster_path, backdrop_path }) => (
   <div>
-  <figure>
-    <figcaption>
-      <img src={posterUrl + poster_path} alt={backdrop_path} />
-      <p className="filmTitle">{original_title}</p>
-    </figcaption>
-  </figure>
-
-
-
+    <figure>
+      <figcaption>
+        <p className="filmTitle">{original_title}</p>
+        <div className="conteneura">
+          <img className="imgcontenu" src={posterUrl + poster_path} alt={backdrop_path} />
+          <div class="txtcontenu">Du texte sur l'image</div>
+        </div>
+      </figcaption>
+    </figure>
   </div>
-
 );
 
 export default Card;
