937a4ef0f7e56923b83420fa6a9b909ff262b34c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-04-29 22:22:53 +0200

    ajustement style css

--- src/Components/Filters/Card.js
+++ src/Components/Filters/Card.js
@@ -4,21 +4,17 @@ import './Card.css';
 const baseUrl = 'https://api.themoviedb.org/3';
 const posterUrl = 'https://image.tmdb.org/t/p/w300/';
 const Card = ({ original_title, poster_path, backdrop_path, overview, vote_average, release_date }) => (
-  <div>
-    <figure>
-      <figcaption>
-        <div className="filmTitle">{original_title}</div>
-        <div className="conteneura">
-          <img className="imgcontenu" src={posterUrl + poster_path} alt={backdrop_path} />
-          <div class="txtcontenu">
-            <div className="color">{'NOTE : ' + vote_average + '/10 . MDB'}</div>
-            <div className="color1">{'CINEMA RELEASE : ' + release_date}</div>
-            <div className="summarize">SUMMARISE :</div>
-            <div>{overview}</div>
-          </div>
-        </div>
-      </figcaption>
-    </figure>
+  <div className="cardfilm">
+    <div className="filmTitle">{original_title}</div>
+    <div className="conteneura">
+      <img className="imgcontenu" src={posterUrl + poster_path} alt={backdrop_path} />
+      <div class="txtcontenu">
+        <div className="color">{'NOTE : ' + vote_average + '/10 . MDB'}</div>
+        <div className="color1">{'CINEMA RELEASE : ' + release_date}</div>
+        <div className="summarize">SUMMARISE :</div>
+        <div>{overview}</div>
+      </div>
+    </div>
   </div>
 );
 
