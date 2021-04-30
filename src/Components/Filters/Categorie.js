347d547ce37622740709374e1cef6299a36f0273
Gregory-Bouteville <gregory.bouteville@orange.fr>
2021-04-30 11:38:51 +0200

    Creation du burgerButton

--- src/Components/Filters/Categorie.js
+++ src/Components/Filters/Categorie.js
@@ -1,52 +1,46 @@
-import React, { useEffect, useState } from "react";
-import "./Categorie.css"
-import axios from "axios";
-import CategorieButton from "./CategorieButton";
-
+import React, { useEffect, useState } from 'react';
+import './Categorie.css';
+import axios from 'axios';
+import CategorieButton from './CategorieButton';
 
 const Categorie = (props) => {
-
-    const {films, setFilmFilteredGenres} = props;
-
-    const [categories, setCategories] = useState([]);
+  const { films, setFilmFilteredGenres } = props;
+  const [categories, setCategories] = useState([]);
 
   const categorieList = () => {
-      axios
-        .get("https://api.themoviedb.org/3/genre/movie/list?api_key=e68df8fb360e2ffe69d4812e69dd24b0&language=fr-FR")
-        .then((response) => response.data)
-        .then ((data) => {
-          setCategories(data.genres);
-        });
-  }
+    axios
+      .get('https://api.themoviedb.org/3/genre/movie/list?api_key=e68df8fb360e2ffe69d4812e69dd24b0&language=fr-FR')
+      .then((response) => response.data)
+      .then((data) => {
+        setCategories(data.genres);
+      });
+  };
 
   useEffect(() => {
     categorieList();
   }, []);
 
-  
-const handlefilterFilmByCategorie = (event, genre_id) => {
-  event.preventDefault();
-  const moviesByGenre =  films
-          .filter((film) =>  film.genre_ids.includes(genre_id))
-  
-  setFilmFilteredGenres(moviesByGenre)
-}
+  const handlefilterFilmByCategorie = (event, genre_id) => {
+    event.preventDefault();
+    const moviesByGenre = films.filter((film) => film.genre_ids.includes(genre_id));
+
+    setFilmFilteredGenres(moviesByGenre);
+  };
 
   return (
     <div>
       <div className="boutonCategorie">
-        {categories
-          .map((categorie, index) => (
-            <CategorieButton key={index} handlefilterFilmByCategorie={handlefilterFilmByCategorie} genre_id={categorie.id} genre_name={categorie.name} />
-          ))
-          }
-
+        {categories.map((categorie, index) => (
+          <CategorieButton
+            key={index}
+            handlefilterFilmByCategorie={handlefilterFilmByCategorie}
+            genre_id={categorie.id}
+            genre_name={categorie.name}
+          />
+        ))}
       </div>
-
     </div>
-
-
-  )
-}
+  );
+};
 
 export default Categorie;
