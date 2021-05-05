cb295a079ee4ecb5d3ded28a0f1efb01940f2fb5
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-05-05 16:25:49 +0200

    creation des cards à propos

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import './AboutUs.css';
+import Contact from './Contact';
 import Headerallpages from '../headerallpages';
 import Footer from '../Footer/Footer';
 
 const paraOne = 'A propos :';
 const description = "Ceci est la description des membres de l'équipe Oovy ";
+const cards = [
+  {
+    src: 'img/Edouard-LARROCHE.png',
+    name: 'Edouard LARROCHE',
+    text: 'Some quick example text to build on the card title and make up the bulk of the card',
+    link_github: 'https://github.com/Edouard-LARROCHE',
+    logo_git: 'img/github.png',
+    link_linkedin: 'https://www.linkedin.com/in/edouard-larroche-9099a6163/',
+    logo_linkedin: 'img/linkedin.png',
+  },
+  {
+    src: 'img/Edouard-LARROCHE.png',
+    name: 'Marie-Anne DUVIEU',
+    text: 'Some quick example text to build on the card title and make up the bulk of the card',
+    link_github: 'https://github.com/Edouard-LARROCHE',
+    logo_git: 'img/github.png',
+    link_linkedin: 'https://www.linkedin.com/in/edouard-larroche-9099a6163/',
+    logo_linkedin: 'img/linkedin.png',
+  },
+  {
+    src: 'img/Edouard-LARROCHE.png',
+    name: 'Kevin TOUYARD',
+    text: 'Some quick example text to build on the card title and make up the bulk of the card',
+    link_github: 'https://github.com/Edouard-LARROCHE',
+    logo_git: 'img/github.png',
+    link_linkedin: 'https://www.linkedin.com/in/edouard-larroche-9099a6163/',
+    logo_linkedin: 'img/linkedin.png',
+  },
+  {
+    src: 'img/Edouard-LARROCHE.png',
+    name: 'Baptiste GRIAS',
+    text: 'Some quick example text to build on the card title and make up the bulk of the card',
+    link_github: 'https://github.com/Edouard-LARROCHE',
+    logo_git: 'img/github.png',
+    link_linkedin: 'https://www.linkedin.com/in/edouard-larroche-9099a6163/',
+    logo_linkedin: 'img/linkedin.png',
+  },
+  {
+    src: 'img/Edouard-LARROCHE.png',
+    name: 'Gregory BOUTEVILLE',
+    text: 'Some quick example text to build on the card title and make up the bulk of the card',
+    link_github: 'https://github.com/Edouard-LARROCHE',
+    logo_git: 'img/github.png',
+    link_linkedin: 'https://www.linkedin.com/in/edouard-larroche-9099a6163/',
+    logo_linkedin: 'img/linkedin.png',
+  },
+];
 
 const AboutUs = () => {
   return (
@@ -14,6 +62,21 @@ const AboutUs = () => {
       <h2 className="text1_aboutus">{paraOne}</h2>
       <div className="contains">
         <p className="description">{description}</p>
+      </div>
+
+      <div className="card-contact">
+        {cards.map((cards, index) => (
+          <Contact
+            key={index}
+            src={cards.src}
+            name={cards.name}
+            text={cards.text}
+            link_github={cards.link_github}
+            logo_git={cards.logo_git}
+            link_linkedin={cards.link_linkedin}
+            logo_linkedin={cards.logo_linkedin}
+          />
+        ))}
       </div>
       <Footer />
     </div>
